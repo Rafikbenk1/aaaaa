6854d16640563600da9a1cee2ac8a79300dcfb74
Rafikbenk1 <104743092+Rafikbenk1@users.noreply.github.com>
2022-05-02 05:30:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-const Eris = require("eris");
-const keepAlive = require("./server");
-keepAlive();
+const express = require('express');
+const app = express();
 
-const bot = new Eris(process.env.TOKEN);
+app.listen(() => console.log('Server started'));
 
-bot.connect();
+app.use('/ping', (req, res) => {
+	res.send(new Date());
+});
+
+app.get('/', function(request, response) {
+	response.sendFile(__dirname + '/index.html');
+});
+
+const Discord = require('discord.js-v11-fixes');
+const cmd = require("node-cmd");
+const ffmpeg = require('ffmpeg');
+const ffmpegstatic = require('ffmpeg-static');
+
+const client = new Discord.Client();
+
+const serverid = "961431024389214268" // id server
+const channelid = "963956638031704074" // id channel
+
+client.on('ready',async () => {
+  console.log("Starting..");
+    let g = client.guilds.get(`${serverid}`);
+    let c = g.channels.get(`${channelid}`);
+      if(c.type === 'voice') {
+       c.join();
+      setInterval(() => {
+        if(g.me.voiceChannel && g.me.voiceChannelID !== c.id || !g.me.voiceChannel) c.join();
+    }, 1);
+      } else {
+    console.log('Failed To Join: \n The Channel Type isn "Listening."')
+  }
+});
+
+client.login(process.env.token);
